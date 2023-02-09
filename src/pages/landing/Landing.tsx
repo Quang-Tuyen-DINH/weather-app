12b5fbc643dd0fe26479ad4472387ccbd66335ec
Quang Tuyen DINH <qtuyen43@gmail.com>
2023-02-09 18:36:41 +0100

    Modified Landing

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import './Landing.scss';
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div className='sesamm-app__landing'>
+      <div className='sesamm-app__landing__title'>
+        <span>DINH Quang Tuyen - Typescript Developer</span>
+      </div>
+      <div className='sesamm-app__landing__informations'>
+        <span>Github: <a href='https://github.com/Quang-Tuyen-DINH/weather-app'>github.com/Quang-Tuyen-DINH/weather-app</a></span>
+        <br />
+        <span>Demo: <a href='https://weather-dinh.vercel.app/'>weather-dinh.vercel.app</a></span>
+        <br />
+        <span>API ID: <a href='https://openweathermap.org/'>openweathermap.org</a></span>
+      </div>
+    </div>
   )
 }
 
